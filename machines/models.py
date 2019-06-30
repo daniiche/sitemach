166676c0499724060a435e501afa4cc9a1acaee9
daniiche <okesone@ya.ru>
2019-06-30 21:52:53 +0500

    Modified templates to display posts from database.

--- machines/models.py
+++ machines/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils import timezone
 
 class Machine(models.Model):
-    machineid = models.UUIDField(primary_key=True)
+    machineid = models.CharField(max_length=200, primary_key=True)
     clientid = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     kind = models.CharField(max_length=200)
--- machines/views.py
+++ machines/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Machine
 
 def machines_list(request):
-    return render(request, 'machines/machines_list.html', {})
+	machines = Machine.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+	return render(request, 'machines/machines_list.html', {'machines': machines})
